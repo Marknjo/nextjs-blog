3b3b396b428101ba8dd8552a0c1fbce77ca794fd
Marknjo <marknjo7@gmail.com>
2021-06-14 16:32:27 +0300

    Events Proj: Display single feature

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,32 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../data/dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 const EventDetailPage = function () {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        imageAlt={event.title}
+        image={event.image}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 };
 
