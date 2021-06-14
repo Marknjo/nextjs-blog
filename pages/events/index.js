53b219ec2675d53d58b1d549eddc974245c2b199
Marknjo <marknjo7@gmail.com>
2021-06-14 16:50:09 +0300

    Events Proj: Showing all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from '../../data/dummy-data';
+import EventList from '../../components/events/event-list';
+
 const AllEventsPage = function () {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All Events Page</h1>
+      <EventList items={events} />
     </div>
   );
 };
