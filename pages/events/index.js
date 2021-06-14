89586d8b95e6da460f4d33001ec25b53a50e4317
Marknjo <marknjo7@gmail.com>
2021-06-14 17:05:21 +0300

    Events Proj: Events Search form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,13 @@
 import { getAllEvents } from '../../data/dummy-data';
 import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/events-search';
 
 const AllEventsPage = function () {
   const events = getAllEvents();
 
   return (
     <div>
+      <EventSearch />
       <EventList items={events} />
     </div>
   );
