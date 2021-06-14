47ca8d9bf97840bb2c08509434f36aae1517564f
Marknjo <marknjo7@gmail.com>
2021-06-14 11:26:40 +0300

    File Routing: Structuring routes based on the folders

---
+++ pages/portfolio/index.js
@@ -0,0 +1,10 @@
+const PortfolioPage = function () {
+  return (
+    <>
+      <h1>My Portfolio</h1>
+      <p>Below is my beautiful collection...</p>
+    </>
+  );
+};
+
+export default PortfolioPage;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,10 @@
+const ListPage = function () {
+  return (
+    <>
+      <h1>Portfolio List</h1>
+      <p>Here are all of the lists</p>
+    </>
+  );
+};
+
+export default ListPage;
