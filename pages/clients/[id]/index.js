648bfec0a486747c86dc99a03bf3f596e818f3ca
Marknjo <marknjo7@gmail.com>
2021-06-14 13:24:41 +0300

    File Routing: Navigating programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,10 +4,25 @@ const ClientProjectsPage = function () {
   const router = useRouter();
 
   console.log(router.query);
+
+  const loadProjectHandler = () => {
+    //router.push('/clients/max/project-a');
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {
+        id: 'max',
+        clientprojectid: 'project-a',
+      },
+    });
+  };
+
   return (
     <>
       <h1>A Client Page</h1>
       <p>All Projects belonging to {router.query.id}</p>
+      <button onClick={loadProjectHandler} type="button">
+        Load Project A
+      </button>
     </>
   );
 };
