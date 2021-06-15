138e5d4e6c7fd0ec5fe4faedc5bb6f2a1a6b37b5
Marknjo <marknjo7@gmail.com>
2021-06-15 14:35:19 +0300

    Main: Completing Lesson on fething data lesson

---
+++ 03.fetching-data/pages/index.js
@@ -0,0 +1,52 @@
+import fs from 'fs/promises';
+import path from 'path';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+function Home(props) {
+  const { products } = props;
+
+  return (
+    <div className={styles.container}>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <Link href={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  console.log('(Re-) Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+
+  const jsonData = await fs.readFile(filePath);
+
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,9 @@
-import fs from 'fs/promises';
-import path from 'path';
 import styles from '../styles/Home.module.css';
-import Link from 'next/link';
 
-function Home(props) {
-  const { products } = props;
-
+export default function Home() {
   return (
     <div className={styles.container}>
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            <Link href={`/products/${product.id}`}>{product.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <h1>Hello Next app</h1>
     </div>
   );
 }
-
-export async function getStaticProps() {
-  console.log('(Re-) Generating...');
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-
-  const jsonData = await fs.readFile(filePath);
-
-  const data = JSON.parse(jsonData);
-
-  if (!data) {
-    return {
-      redirect: {
-        destination: '/',
-      },
-    };
-  }
-
-  if (data.products.length === 0) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      products: data.products,
-    },
-    revalidate: 10,
-  };
-}
-
-export default Home;
