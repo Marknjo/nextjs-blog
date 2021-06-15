ac525b438f444d119360c56ec7cda9fdf9a59ea8
Marknjo <marknjo7@gmail.com>
2021-06-15 09:32:55 +0300

    Data Fetching: Add dummy data to test pre-rendering concept

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello Next app</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
     </div>
   );
 }
