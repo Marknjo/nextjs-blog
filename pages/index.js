acd57f2380bf0ee74d8ec21c0b7984fffa18c8a5
Marknjo <marknjo7@gmail.com>
2021-06-15 09:43:17 +0300

    Data Fetching: Intro to getStatisProps()

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+function Home(props) {
+  const { products } = props;
+
   return (
     <div className={styles.container}>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {products.map(product => (
+          <li key={product.id}>{product.title}</li>
+        ))}
       </ul>
     </div>
   );
 }
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: 'p1', title: 'Product 1' }],
+    },
+  };
+}
+
+export default Home;
