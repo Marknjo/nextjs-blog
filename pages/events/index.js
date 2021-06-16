5be0641b3c533a4a95dd6567dc99d75324b9f14f
Marknjo <marknjo7@gmail.com>
2021-06-16 12:26:41 +0300

    Proj Fetching: Loading events from firebase on the browse all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router';
-import { getAllEvents } from '../../data/dummy-data';
+import { getAllEvents } from '../../helpers/api-utils';
 import EventList from '../../components/events/event-list';
 import EventSearch from '../../components/events/events-search';
 
-const AllEventsPage = function () {
+const AllEventsPage = function (props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const events = props.allEvents;
 
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
@@ -20,5 +20,22 @@ const AllEventsPage = function () {
     </div>
   );
 };
+
+export async function getStaticProps(context) {
+  const allEvents = await getAllEvents();
+
+  if (!allEvents) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      allEvents,
+    },
+    revalidate: 60,
+  };
+}
 
 export default AllEventsPage;
