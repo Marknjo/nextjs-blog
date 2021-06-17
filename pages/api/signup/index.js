b0d93d8ca50377da695a389571c5268c1827d693
Marknjo <marknjo7@gmail.com>
2021-06-17 15:05:34 +0300

    Proj API: Implement saving subscribers email addresses

--- pages/api/signup/index.js
+++ pages/api/signup/index.js
@@ -1,12 +1,31 @@
+import {
+  extractDataFromJsonFile,
+  getFilePath,
+  saveDataToJsonFile,
+} from '../../../helpers/server-utils';
+
 function handler(req, res) {
   if (req.method === 'POST') {
     //handle incoming request
 
     const email = req.body.email;
 
-    //do serverside validations before submitting the email
+    if (!email && !email.includes('@')) {
+      res.status(422).json({ message: 'Email submitted is invalid' });
+      return;
+    }
 
-    res.status(200).json({ ok: true, response: { email } });
+    const reqData = {
+      id: Math.random().toString().slice(2, 15),
+      email,
+    };
+
+    //do serverside validations before submitting the email
+    const filePath = getFilePath('subscribers');
+    const fileData = extractDataFromJsonFile(filePath);
+    saveDataToJsonFile(fileData, filePath, reqData);
+
+    res.status(200).json({ message: 'Success', response: { email } });
   }
 }
 
