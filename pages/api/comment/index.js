99d28c9a1d1191a61aeeea43996bc5b7bf67c98c
Marknjo <marknjo7@gmail.com>
2021-06-17 10:53:09 +0300

    Proj API:Server side error handling for comments submission

--- pages/api/comment/index.js
+++ pages/api/comment/index.js
@@ -4,8 +4,21 @@ function handler(req, resp) {
     const reqEmail = req.body.email;
     const reqName = req.body.name;
     const reqText = req.body.text;
+    const reqEventId = req.body.eventId;
 
     //do some server side validations
+    if (
+      !enteredEmail ||
+      enteredEmail.trim() === '' ||
+      !enteredEmail.includes('@') ||
+      !enteredName ||
+      enteredName.trim() === '' ||
+      !enteredComment ||
+      enteredComment.trim() === ''
+    ) {
+      resp.status(400).json({ ok: false });
+      return;
+    }
 
     //save the data to database
 
@@ -17,6 +30,7 @@ function handler(req, resp) {
         email: reqEmail,
         name: reqName,
         text: reqText,
+        eventId: reqEventId,
       },
     });
   }
