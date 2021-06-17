0755624b4e065bc59f1213d70ef9edd7870cf4b8
Marknjo <marknjo7@gmail.com>
2021-06-17 09:43:09 +0300

    Proj API: Logic for handling email signup

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,24 +1,51 @@
+import { useRef, useState } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const emailInput = useRef();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+  async function registrationHandler(event) {
+    try {
+      event.preventDefault();
+
+      // fetch user input (state or refs)
+      const enteredEmail = emailInput.current.value.trim().toLowerCase();
+      // optional: validate input
+      if (!enteredEmail) {
+        throw new Error('Please enter your email address to proceed');
+      }
+
+      if (!enteredEmail.includes('@')) {
+        throw new Error('Please enter a valid email');
+      }
+      // send valid data to API
+
+      //form has no error & reset the input
+      emailInput.current.value = '';
+      setHasError(false);
+      setErrorMessage('');
+    } catch (error) {
+      setHasError(true);
+      setErrorMessage(error.message);
+    }
   }
 
   return (
     <section className={classes.newsletter}>
       <h2>Sign up to stay updated!</h2>
+      {errorMessage && (
+        <p className={classes['error-message-box']}>⛔ {errorMessage}</p>
+      )}
       <form onSubmit={registrationHandler}>
-        <div className={classes.control}>
+        <div className={`${classes.control} ${hasError ? classes.error : ''}`}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
+            ref={emailInput}
           />
           <button>Register</button>
         </div>
