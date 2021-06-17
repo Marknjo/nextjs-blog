1c366ec1008e83734765eaff6c5bd9ec4b3bbebc
Marknjo <marknjo7@gmail.com>
2021-06-17 09:10:04 +0300

    Proj API: Adding events project starter files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-import styles from '../styles/Home.module.css';
+import Head from 'next/head';
 
-export default function Home() {
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
+
+function HomePage(props) {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next app</h1>
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={props.events} />
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
