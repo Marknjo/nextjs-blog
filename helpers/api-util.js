849011d474674680b25a0cd8433a6a645cff15f0
Marknjo <marknjo7@gmail.com>
2021-06-17 09:17:59 +0300

    Proj API: Update api utils code format

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,38 +1,79 @@
-export async function getAllEvents() {
-  const response = await fetch('https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json');
-  const data = await response.json();
+export const firebase_url =
+  'https://react-http-be337-default-rtdb.firebaseio.com/eventsApp';
 
-  const events = [];
+export function transformData(data) {
+  //transform data
+  let transformedData = [];
 
   for (const key in data) {
-    events.push({
+    transformedData.push({
       id: key,
-      ...data[key]
+      ...data[key],
     });
   }
 
-  return events;
+  return transformedData;
 }
 
-export async function getFeaturedEvents() {
-  const allEvents = await getAllEvents();
-  return allEvents.filter((event) => event.isFeatured);
-}
-
-export async function getEventById(id) {
-  const allEvents = await getAllEvents();
-  return allEvents.find((event) => event.id === id);
-}
-
-export async function getFilteredEvents(dateFilter) {
-  const { year, month } = dateFilter;
-
-  const allEvents = await getAllEvents();
-
-  let filteredEvents = allEvents.filter((event) => {
+export function filterEvents(events, year, month) {
+  let filteredEvents = events.filter(event => {
     const eventDate = new Date(event.date);
-    return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
   });
 
   return filteredEvents;
+}
+
+export async function getAllEvents() {
+  try {
+    const response = await fetch(`${firebase_url}/events.json`);
+
+    if (!response.ok) {
+      throw new Error('Something wrong with fetching data!');
+    }
+
+    const data = await response.json();
+
+    //transform data
+
+    return transformData(data);
+  } catch (error) {
+    //handle erros
+    console.error(`💥💥💥💥 ${error}`);
+
+    throw error;
+  }
+}
+
+export async function getFeaturedEvents() {
+  try {
+    const events = await getAllEvents();
+
+    return events.filter(event => event.isFeatured);
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getEventById(id) {
+  try {
+    const events = await getAllEvents();
+    return events.find(event => event.id === id);
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getFilteredEvents(dateFilter) {
+  try {
+    const { year, month } = dateFilter;
+
+    const events = await getAllEvents();
+
+    return filteredEvents(events, year, month);
+  } catch (error) {
+    throw error;
+  }
 }
