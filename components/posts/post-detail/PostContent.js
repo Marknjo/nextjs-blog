1ae547bb0c875cb2d8eec8f5c2d4c51400506f89
Marknjo <marknjo7@gmail.com>
2021-06-18 13:12:21 +0300

    Proj Blog: Rendering single post by extracting data from markdown

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,27 +1,20 @@
 import PostHeader from './PostHeader';
 import ReactMarkdown from 'react-markdown';
 import styles from './PostContent.module.css';
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Beginner guide to NextJs',
-  image: 'getting-started-nextjs.png',
-  excerpt:
-    'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
-  date: '2021-06-17',
-  content: '# This is a first post',
-};
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={styles.content}>
       <PostHeader
         className={styles.image}
         image={imagePath}
-        title={DUMMY_POST.title}
+        title={post.title}
       />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,33 @@
 import PostContent from '../../components/posts/post-detail/PostContent';
+import { getPostData, getPostsSlugs } from '../../lib/posts-utils';
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
 }
 
 export default PostDetailPage;
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postsSlug = getPostsSlugs();
+
+  const pathsMap = postsSlug.map(slug => ({ params: { slug } }));
+
+  return {
+    paths: pathsMap,
+    fallback: false,
+  };
+}
