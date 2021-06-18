e7d71e18dce1be0030a9c087fd5cb41519af56cf
Marknjo <marknjo7@gmail.com>
2021-06-18 11:44:24 +0300

    Proj Blog: Code for all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,42 @@
-function AllPostsPage() {}
+import AllPosts from '../../components/posts/AllPosts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
