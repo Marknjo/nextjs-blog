62c50936811dcd1307b16f7106101c040cbe7427
Marknjo <marknjo7@gmail.com>
2021-06-18 15:33:23 +0300

    Proj Blog: Customizing image rendering

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,11 +1,48 @@
 import PostHeader from './PostHeader';
 import ReactMarkdown from 'react-markdown';
 import styles from './PostContent.module.css';
+import Image from 'next/image';
 
 function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customComponets = {
+    // img: image => {
+    //   console.log();
+
+    //   return (
+    //     <Image
+    //       width={600}
+    //       height={300}
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       placeholder="blur"
+    //     />
+    //   );
+    // },
+    p: paragraph => {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0].properties;
+        return (
+          <div className={styles.image}>
+            <Image
+              width={600}
+              height={300}
+              src={`/images/posts/${post.slug}/${image.src}`}
+              alt={image.alt}
+              placeholder="blur"
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
 
   return (
     <article className={styles.content}>
@@ -14,7 +51,7 @@ function PostContent(props) {
         image={imagePath}
         title={post.title}
       />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponets} children={post.content} />
     </article>
   );
 }
