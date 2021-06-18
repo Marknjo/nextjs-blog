dd449fc7934ce92b8ab02e622cfab2e76241bf97
Marknjo <marknjo7@gmail.com>
2021-06-18 11:33:12 +0300

    Proj Blog: Logic tied to displaying Featured section

--- components/home-page/FeaturedPosts.js
+++ components/home-page/FeaturedPosts.js
@@ -1,7 +1,13 @@
 import styles from './FeaturedPosts.module.css';
+import PostGrid from '../posts/PostGrid';
 
-function FeaturedPosts() {
-  return <p>Featured posts</p>;
+function FeaturedPosts(props) {
+  return (
+    <section className={styles.latest}>
+      <h2>Featured Section</h2>
+      <PostGrid posts={props.posts} />
+    </section>
+  );
 }
 
 export default FeaturedPosts;
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,46 @@ import FeaturedPosts from '../components/home-page/FeaturedPosts';
 //1) Hero => Present ourselves
 //2) Featured posts
 
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Beginner guide to NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJs is a React Framework for production - it makes building fullstack React apps and site a breeze and it ships with built-in SSR.',
+    date: '2021-06-17',
+  },
+];
+
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 }
